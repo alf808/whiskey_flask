cabcc3c5859e37800ecec35493c9601a1d9f7edf
alf lang <nemo@pop-os>
2021-11-23 12:41:28 -1000

    artist search

--- app.py
+++ app.py
@@ -136,8 +136,8 @@ def search_venues():
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
     search_term = request.form.get('search_term', '')
-    content = Venue.query.filter(Venue.name.ilike(f"%{search_term}%")).all()
-    response = {'count': len(content), 'data': content}
+    data = Venue.query.filter(Venue.name.ilike(f"%{search_term}%")).all()
+    response = {'count': len(data), 'data': data}
     return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
   # response={
@@ -268,11 +268,10 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-    data = []
-    content = Artist.query.all()
-    for artist in content:
-        data.append({'id': artist.id, 'name': artist.name})
-    return render_template('pages/artists.html', artists=data)    
+    data = Artist.query.all()
+    # for artist in content:
+    #     data.append({'id': artist.id, 'name': artist.name})
+    return render_template('pages/artists.html', artists=data)
   # data=[{
   #   "id": 4,
   #   "name": "Guns N Petals",
@@ -290,15 +289,20 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    search_term = request.form.get('search_term', '')
+    data = Artist.query.filter(Artist.name.ilike(f"%{search_term}%")).all()
+    response = {'count': len(data), 'data': data}
+    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))    
+    
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 4,
+  #     "name": "Guns N Petals",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
+
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
