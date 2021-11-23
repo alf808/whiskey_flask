4320599ca52206424d91ca7f1d60f15b54a7bf37
alf lang <nemo@pop-os>
2021-11-23 12:32:36 -1000

    venues search

--- app.py
+++ app.py
@@ -132,18 +132,23 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+  # TODO: implement search on venues with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+    search_term = request.form.get('search_term', '')
+    content = Venue.query.filter(Venue.name.ilike(f"%{search_term}%")).all()
+    response = {'count': len(content), 'data': content}
+    return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 2,
+  #     "name": "The Dueling Pianos Bar",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
+
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
